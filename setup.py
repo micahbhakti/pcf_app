251aab17db6d9264a33e3b0ba32b6ac3e18587a4
micahbhakti <micahbhakti@gmail.com>
2018-08-02 08:19:39 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='pcf_app',
+   version='1.0',
+   description='applicaiton for pcf',
+   author='Micah Bhakti',
+   author_email='micahbhakti@gmail.com',
+
+)
