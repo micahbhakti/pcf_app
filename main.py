0a586d874c92de7d6724ba7d8c730f628e1fc140
micahbhakti <micahbhakti@gmail.com>
2018-08-02 09:40:10 -0700

    added flask

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 #pcf application
 
-def main():
-    from flask import Flask, render_template, Markup
-    import os
-    import json
+from flask import Flask, render_template, Markup
+import os
+import json
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    COLOR = "#3c24c1"
+COLOR = "#3c24c1"
 
-    # get environmental variables
-    port = int(os.getenv("PORT"))
+# get environmental variables
+port = int(os.getenv("PORT"))
 
-    VCAP_APPLICATION = json.loads(os.environ['VCAP_APPLICATION'])
-    app_instance = int(VCAP_APPLICATION['instance_index'])
-    app_guid = VCAP_APPLICATION['application_id']
-    app_name = VCAP_APPLICATION['application_name']
+VCAP_APPLICATION = json.loads(os.environ['VCAP_APPLICATION'])
+app_instance = int(VCAP_APPLICATION['instance_index'])
+app_guid = VCAP_APPLICATION['application_id']
+app_name = VCAP_APPLICATION['application_name']
 
-    content = '<h1>Hello World</h1></br>I am instance <strong>#' + str(
-        app_instance) + '</strong> serving application <strong>' + app_name + '</strong> with GUID <strong> ' + app_guid + ' !'
-    content = Markup(content)
+content = '<h1>Hello World!</h1></br>I am instance <strong>#' + str(app_instance) + '</strong> serving application <strong>' + app_name + '</strong> with GUID <strong> ' + app_guid + ' !'
+content = Markup(content)
 
-    @app.route('/')
-    def hello_world():
-        return render_template("index.html", bgcolor=COLOR, content=content)
+@app.route('/')
+def hello_world():
+    return render_template("index.html",bgcolor=COLOR,content=content)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
